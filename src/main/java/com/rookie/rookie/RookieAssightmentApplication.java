f6c32aab1b685237a47a1259e1f90462ac82c8fb
TranCongNguyen <congnguyen.230901@gmail.com>
2021-10-10 10:07:11 +0700

    first commit

---
+++ src/main/java/com/rookie/rookie/RookieAssightmentApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.rookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieAssightmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieAssightmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rookie/rookie/RookieAssightmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.rookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieAssightmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

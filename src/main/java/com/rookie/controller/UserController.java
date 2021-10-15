987c122fc9b71d53c6bfd00db7aeab2016373cfe
TranCongNguyen <congnguyen.230901@gmail.com>
2021-10-15 13:49:39 +0700

    new Project

--- src/main/java/com/rookie/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.rookie.controller;
-
-public class UserController {
-}
--- src/main/java/com/rookie/entity/User.java
+++
@@ -1,8 +0,0 @@
-package com.rookie.entity;
-
-
-public class User {
-    int id;
-    String name;
-    int age;
-}
